433f699f0d0c9d380bb9a6fb40c96d0aa34f02af
taltal-source <0taltalsource@gmail.com>
2024-08-12 01:09:07 +0900

    djangoの拡張機能設定

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "coffee.apps.CoffeeConfig",
+    "widget_tweaks",
+    "coffee.apps.CoffeeConfig"
 ]
 
 MIDDLEWARE = [
